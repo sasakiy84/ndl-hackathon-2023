adede908dda918f35085e3dbfe531eb940f6218d
erikka-22 <iwasaki.pingoo@gmail.com>
2023-02-14 23:31:48 +0900

    add scorelist component

---
+++ src/components/ScoreList.vue
@@ -0,0 +1,44 @@
+<script setup>
+import { useQuestionStore } from "../stores/question";
+
+const questionStore = useQuestionStore();
+const totalQuestionNumber = questionStore.totalQuestionNum;
+const answers = questionStore.questionIds;
+const scores = questionStore.scores;
+
+</script>
+
+<template>
+  <ul>
+    <li v-for="n in totalQuestionNumber">
+      <p class="question-number-text">Q.{{ n }}</p>
+      <p class="answer-text">{{ answers[n - 1] }}</p>
+      <p class="score-text">{{ scores[n - 1] }}点</p>
+    </li>
+  </ul>
+</template>
+
+<style scoped>
+  ul {
+    padding-left: 0;
+  }
+
+  li {
+    list-style: none;
+    display: flex;
+    border-bottom: 0.1rem solid;
+  }
+
+  .question-number-text {
+    width: 5rem;
+    margin-left: 2rem;
+  }
+
+  .answer-text {
+    width: 20rem;
+  }
+
+  .score-text {
+    width: 5rem;
+  }
+</style>
--- src/views/ScoreView.vue
+++ src/views/ScoreView.vue
@@ -1,16 +1,26 @@
+<script setup>
+import { RouterLink } from "vue-router";
+import { ref } from "vue";
+import { useQuestionStore } from "../stores/question";
+import ScoreList from "../components/ScoreList.vue"
+
+const questionStore = useQuestionStore();
+const currentQuestionNumber = questionStore.currentQuestionNum;
+const totalQuestionNumber = questionStore.totalQuestionNum;
+const totalScore = questionStore.totalScore;
+const answer = questionStore.questionIds[currentQuestionNumber];
+  
+</script>
+
 <template>
   <div class="body">
     <main>
-      <h1>あなたの得点は{{ totalScore }}点です！</h1>
+      <h1 class="total-score-text">あなたの得点は{{ totalScore }}点です！</h1>
       <div class="user-inputs-container">
         <p>レベル</p>
       </div>
       <p>わかったのはどの人物？チェックしておこう</p>
-      <ul>
-        <li v-for="question in questions">
-          {{ question }}
-        </li>
-      </ul>
+      <ScoreList />
       <div class="user-inputs-container flexbox">
         <img src="../assets/japansearch.gif" class="jps-gif">
         <article class="explain-jps">
@@ -20,7 +30,7 @@
         </article>
       </div>
       <RouterLink to="/question?q=1" tag="button" class="show-answer-link">もう一回遊ぶ</RouterLink>
-      <RouterLink to="/home">トップページに戻る</RouterLink>
+      <RouterLink to="/">トップページに戻る</RouterLink>
     </main>
     <div style="text-align: center; margin-bottom: 15px">
       2023©Japan Search Hackathon Team-B
@@ -28,19 +38,6 @@
   </div>
 </template>
 
-<script setup>
-import { RouterLink } from "vue-router";
-import { ref } from "vue";
-import { useQuestionStore } from "../stores/question";
-
-const questionStore = useQuestionStore();
-const currentQuestionNumber = questionStore.currentQuestionNum;
-const totalQuestionNumber = questionStore.totalQuestionNum;
-const totalScore = questionStore.totalScore;
-const answer = questionStore.questionIds[currentQuestionNumber];
-const questions = questionStore.questionIds;
-  
-</script>
 
 <style scoped>
   .body {
@@ -64,104 +61,6 @@ const questions = questionStore.questionIds;
     margin-right: 15px;
   }
   
-  .progress-bar {
-    width: 100%;
-    height: 6px;
-    border-radius: 10px;
-    appearance: none;
-  }
-  .progress-bar::-webkit-progress-bar {
-    background-color: #ece2b0;
-    border-radius: 6px;
-  }
-  .progress-bar::-webkit-progress-value {
-    background-color: #e6c620;
-    border-radius: 6px;
-  }
-  .progress-bar::-moz-progress-bar {
-    background-color: #ece2b0;
-    border-radius: 6px;
-  }
-  
-  .progress-text {
-    margin-top: 10px;
-  }
-  
-  .img-wrapper {
-    width: 100%;
-    height: 385px;
-    background: #999898;
-    border-radius: 12px;
-  }
-  
-  .hint-img {
-    width: 100%;
-    height: 100%;
-    object-fit: contain;
-  }
-  
-  .answer-input-wrapper {
-    width: 100%;
-    display: flex;
-    justify-content: center;
-    margin-bottom: 20px;
-    margin-top: 20px;
-  }
-  
-  .answer-input {
-    width: 100%;
-    padding: 10px;
-    height: 44px;
-    box-sizing: border-box;
-    background: #fdfdfd;
-    border: 1px solid #aaaaaa;
-    border-radius: 6px;
-  }
-  
-  .select-input-wrapper {
-    width: 100%;
-    margin-bottom: 20px;
-    margin-top: 20px;
-  }
-  
-  .select-button-wrapper {
-    display: flex;
-    justify-content: flex-start;
-    flex-wrap: wrap;
-  }
-  .select-button {
-    font-weight: 500;
-    font-size: 16px;
-    margin-right: 10px;
-    height: 47px;
-    border: 1px solid #38322c;
-    border-radius: 6px;
-    padding: 12px 24px;
-    width: max-content;
-    margin-bottom: 16px;
-  }
-  
-  .select-button.selected {
-    background: #38322c;
-    color: #ffffff;
-    cursor: default;
-  }
-  
-  .img-buttons {
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    margin: 10px 30px 20px;
-  }
-  
-  .img-buttons button {
-    margin-right: 30px;
-  }
-  
-  .img-buttons button:last-child {
-    margin-right: 0;
-  }
-  
   button {
     background-color: transparent;
     border: none;
@@ -169,41 +68,6 @@ const questions = questionStore.questionIds;
     outline: none;
     padding: 0;
     appearance: none;
-  }
-  
-  .long-round-button {
-    background: #fdfdfd;
-    border: 1px solid #38322c;
-    color: #38322c;
-    border-radius: 50px;
-    padding: 12px 24px;
-  }
-  
-  .hint-button {
-    background: #fdfdfd;
-    border: 1px solid #38322c;
-    color: #38322c;
-    border-radius: 50px;
-    padding: 6px 24px;
-    font-size: 12px;
-  }
-  
-  .round-button {
-    background: #fdfdfd;
-    border: 1px solid #38322c;
-    color: #38322c;
-    border-radius: 50px;
-    width: 37px;
-    height: 37px;
-  }
-  
-  .round-button.disabled {
-    background: #4f4f4f;
-    color: #ffffff;
-  }
-  
-  .next-button {
-    margin-right: 20px;
   }
   
   /* user input */
@@ -232,38 +96,17 @@ const questions = questionStore.questionIds;
     margin: 50px auto 0;
   }
   
-  .text-hint-container {
-    background: #f0eeec;
-    border-radius: 6px;
-    width: 100%;
-    padding: 16px 19px;
-    margin-top: 20px;
-    box-sizing: border-box;
-  }
-  
-  .text-hint-title {
-    margin: 0;
-    font-style: normal;
-    font-weight: 500;
-    font-size: 18px;
-    line-height: 26px;
-    color: #38322c;
-  }
-  
-  .text-hint-text {
-    font-weight: 400;
-    font-size: 14px;
-    line-height: 160%;
-    color: #38322c;
-  }
-
   .flexbox {
     display: flex;
   }
 
   .jps-gif {
-    width: 100px;
-    height: 100px;
+    width: 20%;
+    height: 50%;
+  }
+
+  .total-score-text {
+    text-align: center;
   }
   </style>
   
